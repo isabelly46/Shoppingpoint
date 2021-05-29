a7e597115019cdc974032a050e4f2af9618e8cf9
isabelly46 <74911282+isabelly46@users.noreply.github.com>
2021-05-28 22:45:10 -0400

    save
    Co-authored-by: Miguel Junior <miguelarcjr@users.noreply.github.com>
    Co-authored-by: GuilhermeDes <GuilhermeDes@users.noreply.github.com>

--- views/AppCPF.tsx
+++ views/AppCPF.tsx
@@ -4,19 +4,62 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
  
 import AppList from './AppList';
 import AppForm from './AppForm';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
  
 const {Navigator, Screen} = createBottomTabNavigator();
  
 function AppCPF(){
-    const nome = 'Isabelly'
+    const [text, onChangeText] = React.useState("");
+    const styles = StyleSheet.create({
+        input: {
+          height: 40,
+          margin: 12,
+          textAlign: 'center'
+        },
+        container: {
+            flex: 1,
+            padding: 24,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#210661'
+        },
+        main: {
+            width: '95%',
+            height: '50%',
+            borderRadius: 4,
+            backgroundColor: '#ffff'
+        },
+        tinyLogo: {
+            width: 50,
+            height: 50
+        },
+      });
+
     return (
         <> 
-            <View> 
-            <Text>Shopping Point{nome}</Text>
+            <View style={styles.container}> 
+            <View style={styles.main}>
+                <Image
+                    style={styles.tinyLogo}
+                    source={{
+                    uri: 'https://reactnative.dev/img/tiny_logo.png',
+                    }}
+                />
+                <Text>Shopping Point</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onChangeText}
+                    value={text}
+                    placeholder="CPF"
+                    keyboardType="numeric"
+                />
+            </View>
             </View>     
         </>
     );
+
+    
 }
  
 export default AppCPF;
